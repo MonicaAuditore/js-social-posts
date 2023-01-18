ed32257f42ce542de7f59665eafd0b83bf3a8816
MonicaAuditore <mauditore@live.it>
2023-01-18 16:39:12 +0100

    fix_ordine codice

--- js/script.js
+++ js/script.js
@@ -36,116 +36,46 @@ const posts = [
     likes: 78,
     created: "2021-05-15",
   },
-  {
-    id: 4,
-    content:
-      "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-    media: "https://unsplash.it/600/400?image=24",
-    author: {
-      name: "Luca Formicola",
-      image: null,
-    },
-    likes: 56,
-    created: "2021-04-03",
-  },
-  {
-    id: 5,
-    content:
-      "Placeat libero ipsa nobis ipsum quibusdam quas harum ut. Distinctio minima iusto. Ad ad maiores et sint voluptate recusandae architecto. Et nihil ullam aut alias.",
-    media: "https://unsplash.it/600/400?image=534",
-    author: {
-      name: "Alessandro Sainato",
-      image: "https://unsplash.it/300/300?image=29",
-    },
-    likes: 95,
-    created: "2021-03-05",
-  },
 ];
 
-//-------------------------------------------------------------------
-// creo un array di immagini
+// Arrey di dati
+
 immaginiArray = [];
+authorArray = [];
+contentArray = [];
+mediaArray = [];
+likesArray = [];
+createdArray = [];
 
 for (let i = 0; i < posts.length; i++) {
-  console.log(posts[i].author["image"]);
   immaginiArray.push(posts[i].author["image"]);
+  authorArray.push(posts[i].author["name"]);
+  contentArray.push(posts[i].content);
+  mediaArray.push(posts[i].media);
+  likesArray.push(posts[i].likes);
+  createdArray.push(posts[i].created);
 }
 console.log(immaginiArray);
+console.log(authorArray);
+console.log(contentArray);
+console.log(mediaArray);
+console.log(likesArray);
+console.log("createdArray", createdArray);
 
 const imgAutore = document.querySelector(".post-meta__icon");
-
 imgAutore.innerHTML += `<img class="profile-pic" src="${immaginiArray[0]}" alt="Phil Mangione"/>`;
-
-//-------------------------------------------------------------------
-// creo un array di nomi author
-authorArray = [];
-
-for (let i = 0; i < posts.length; i++) {
-  console.log(posts[i].author["name"]);
-  authorArray.push(posts[i].author["name"]);
-}
-console.log(authorArray);
 
 const nameAutore = document.querySelector(".post-meta__author");
 nameAutore.innerHTML += `${authorArray[0]}`;
 
-//-------------------------------------------------------------------
-// creo un array di content
-contentArray = [];
-
-for (let i = 0; i < posts.length; i++) {
-  console.log(posts[i].author["name"]);
-  contentArray.push(posts[i].content);
-}
-console.log(contentArray);
-
 const contentPost = document.querySelector(".post__text");
 contentPost.innerHTML += `${contentArray[0]}`;
-
-//-------------------------------------------------------------------
-// creo un array media
-
-mediaArray = [];
-
-for (let i = 0; i < posts.length; i++) {
-  console.log(posts[i].author["name"]);
-  mediaArray.push(posts[i].media);
-}
-console.log(mediaArray);
 
 const media = document.querySelector(".post__image");
 media.innerHTML += `<img src="${mediaArray[0]}" alt="" />`;
 
-//-------------------------------------------------------------------
-// creo un array numero like
-
-likesArray = [];
-
-for (let i = 0; i < posts.length; i++) {
-  console.log(posts[i].likes);
-  likesArray.push(posts[i].likes);
-}
-console.log(likesArray);
-
 const likes = document.querySelector(".js-likes-counter");
 likes.innerHTML += `${likesArray[0]}`;
 
-//-------------------------------------------------------------------
-// creo un array di date
-
-createdArray = [];
-
-for (let i = 0; i < posts.length; i++) {
-  console.log(posts[i].created);
-  createdArray.push(posts[i].created);
-}
-console.log("createdArray", createdArray);
-
 const created = document.querySelector(".post-meta__time");
 created.innerHTML += `${createdArray[0]}`;
-
-//-------------------------------------------------------------------
-// creo una funzione che calcoli quanto tempo fa è stato scritto il post
-/*prendo la data di oggi*/
-
-function myDateFunction() {}
